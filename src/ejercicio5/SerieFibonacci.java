4e1b8277456f662b53c648038be154df21980637
Unknown <luismiguel92_@hotmail.com>
2018-02-24 09:09:53 -0500

    Actualizacion Archivos 3

--- src/ejercicio5/SerieFibonacci.java
+++ src/ejercicio5/SerieFibonacci.java
@@ -4,11 +4,34 @@
  * and open the template in the editor.
  */
 package ejercicio5;
-
+import java.util.Scanner;
 /**
  *
  * @author lenovo
  */
 public class SerieFibonacci {
+    
+    public static void main(String[] args){
+        
+        Scanner entrada = new Scanner(System.in);
+        int num,serie1,serie2,i;
+        do{
+            System.out.print("Digite el numero hasta donde quiera la serie: ");
+            num = entrada.nextInt();
+        }while(num<=1);
+        System.out.println("Los " + num + "numeros de la serie son los siguientes:"); 
+
+        serie1=1;
+        serie2=1; 
+
+        System.out.print(serie1 + " ");
+        for(i=2;i<=num;i++){
+             System.out.print(serie2 + " ");
+             serie2 = serie1 + serie2;
+             serie1 = serie2 - serie1;
+        }
+        System.out.println();
+    }
+    
     
 }
